057ae791170afa6f826e6d5a30029d6980c6d0f0
GuilhermeAlves-prog <datedevtx@gmail.com>
2023-01-14 22:20:11 -0300

    feat: :rocket: exemple for use function map

---
+++ exemplo-Map/src/App.jsx
@@ -0,0 +1,46 @@
+
+function App() {
+  const users = [
+    {
+      name: 'Matheus',
+      age: 21,
+      isAdmin: true
+    },
+    {
+      name: 'Antonio',
+      age: 27,
+      isAdmin: true
+    },
+    {
+      name: 'Marcela',
+      age: 54,
+      isAdmin: false
+    },
+    {
+      name: 'Patricia',
+      age: 24,
+      isAdmin: false
+    },
+    {
+      name: 'Diego',
+      age: 28,
+      isAdmin: true
+    },
+    {
+      name: 'Larissa',
+      age: 22,
+      isAdmin: true
+    }
+  ]
+  return (
+    users.map(user => {
+      <div key={user.name}>
+        <div>nome: {user.name}</div>
+        <div>idade: {user.age}</div>
+        <div>é ADM : {user.isAdmin ? "SIM" : "NÃO"}</div>
+      </div>
+    })
+  )
+}
+
+export default App
