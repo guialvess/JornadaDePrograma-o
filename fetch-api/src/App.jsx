a19ca313ad5cfbd1bf58fe6dd0a234d05060c95c
GuilhermeAlves-prog <datedevtx@gmail.com>
2023-01-19 00:31:50 -0300

    feat: :zap: render data in screen

--- fetch-api/src/App.jsx
+++ fetch-api/src/App.jsx
@@ -1,13 +1,26 @@
-
+import { useEffect, useState } from "react"
+import {Fragment} from 'react'
 
 function App() {
-  fetch("https://reqres.in/api/users?page=2")
+  const [userdata, setUserdata] = useState()
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?page=2")
     .then(response => response.json())
-    .then(response => console.log(response.data))
+    .then(response => setUserdata(response.data))
+  },[])
+  
   return (
-   <div>
-    
-   </div>
+    <Fragment>
+      {userdata ? 
+      userdata.map(user => 
+        <div key={user.id}>
+          <div>nome: {user.first_name}</div>
+          <div>nome: {user.email}</div>
+          <div>------------------------</div>
+        </div>
+      ) : <div>Carregando...</div>}
+    </Fragment>
   )
 }
 
